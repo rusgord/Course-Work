64f13e22dbe0495e2d5140f2d6c39037c1be856d
rusgord <149193817+rusgord@users.noreply.github.com>
2024-05-06 00:59:10 +0300

    Розділ 4 06.05.2024

--- ProjectDelivery/ProjectDelivery/Controllers/HomeController.cs
+++ ProjectDelivery/ProjectDelivery/Controllers/HomeController.cs
@@ -1,38 +1,38 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using ProjectDelivery.Models;
-using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using ProjectDelivery.Models;
+    using System.Diagnostics;
 
-namespace ProjectDelivery.Controllers
-{
-    public class HomeController : Controller
+    namespace ProjectDelivery.Controllers
     {
-        private readonly ILogger<HomeController> _logger;
+        public class HomeController : Controller
+        {
+            private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Index(TrackingModel trckmd)
-        {
-            if (ModelState.IsValid)
+            public HomeController(ILogger<HomeController> logger)
             {
-                return RedirectToAction("Tracking", "Service", new { trackingModel = trckmd });
+                _logger = logger;
             }
-            return View(trckmd);
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            public IActionResult Index()
+            {
+                return View();
+            }
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public IActionResult Index(TrackingModel trckmd)
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Tracking", "Service", new { trackingModel = trckmd });
+                }
+                return View(trckmd);
+            }
+
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            public IActionResult Error()
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
-}
