7617bdd0c82607e3b5bf8cb54e18243028b0b7c0
rusgord <149193817+rusgord@users.noreply.github.com>
2024-05-16 15:46:39 +0300

    Відредаговано

---
+++ ProjectDelivery/Project Test/ArticleTest.cs
@@ -0,0 +1,23 @@
+﻿using ProjectDelivery.Models;
+using Microsoft.AspNetCore.Identity;
+using ProjectDelivery.Enums;
+
+namespace Project_Test
+{
+    public class ArticleTest
+    {
+        [Fact]
+        public void ArticleModel()
+        {
+            var article = new ArticleModel
+            {
+                Id = 1,
+                Title = "Test title",
+                Description = "Some text"
+            };
+            Assert.Equal(1, article.Id);
+            Assert.Equal("Test title", article.Title);
+            Assert.Equal("Some text", article.Description);
+        }
+    }
+}
